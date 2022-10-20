f46c4d61827efb93b9b5bc21fb82804ffc533722
255946 <115736596+255946@users.noreply.github.com>
2022-10-20 15:01:57 -0400

    ship speed and movement

--- game.py
+++ game.py
@@ -23,6 +23,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             # Watch for keyboard and mouse events.
     def _check_events(self):
@@ -30,6 +31,18 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type==pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                        if event.key == pygame.K_RIGHT:
+                            self.ship.moving_right = False
+                        elif event.key == pygame.K_LEFT:
+                            self.ship.moving_left=False
+                        #Move the ship to the right.
+                        self.ship.rect.x +=1
 
             # Redraw the screen during each pass through the loop.
     def _update_screen(self):
