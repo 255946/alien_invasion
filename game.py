f7fc14b3e3356c91cb0a2484aedb643d079b485b
255946 <115736596+255946@users.noreply.github.com>
2022-10-21 14:17:49 -0400

    clean up code and add q to exit

--- game.py
+++ game.py
@@ -32,17 +32,25 @@ class AlienInvasion:
                 if event.type==pygame.QUIT:
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = True
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = True
+                    self._check_keydown_events(event)
                 elif event.type == pygame.KEYUP:
-                        if event.key == pygame.K_RIGHT:
-                            self.ship.moving_right = False
-                        elif event.key == pygame.K_LEFT:
-                            self.ship.moving_left=False
-                        #Move the ship to the right.
-                        self.ship.rect.x +=1
+                    self._check_keyup_events(event)
+    def _check_keydown_events(self, event):
+        """Respond to keypress."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left=False
+            #Move the ship to the right.
+            self.ship.rect.x +=1
 
             # Redraw the screen during each pass through the loop.
     def _update_screen(self):
